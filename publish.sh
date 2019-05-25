ac3fcc16d71550dec16d1b446c0c6ddcfbc27a70
Arnaud Bos <arnaud@arnaudbos.com>
2019-05-25 23:31:41 +0200

    Upgrade publish script

--- publish.sh
+++ publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 rm -rf img/
-(cd ..; hugo --buildDrafts)
-rm -rf code/
+(cd ..; docker run -it --rm --volume=$(pwd):/srv --volume=$(pwd)/../hugo-vitae:/srv/themes/hugo-vitae registry.gitlab.com/pages/hugo:latest hugo -D -s srv -d public/)
 git add --all
 git commit -m 'Update and publish'
 git push origin master
