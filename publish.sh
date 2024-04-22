d565f81e4781eb7573c6b0eb28f2ca186a92bca6
Arnaud Bos <arnaud@arnaudbos.com>
2024-04-22 09:54:07 +0200

    Backup

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf img/
-(cd ..; docker run -it --rm --volume=$(pwd):/srv --volume=$(pwd)/../hugo-vitae:/srv/themes/hugo-vitae registry.gitlab.com/pages/hugo:latest hugo -D -s srv -d public/)
+(cd ..; docker run -it --rm --workdir /srv --volume=$(pwd):/srv --volume=$(pwd)/../hugo-vitae:/srv/themes/hugo-vitae registry.gitlab.com/pages/hugo@sha256:017b3e0d3620cf4a7d782656237b7aa8793909339d927c15dcd5bdd26180a849 hugo -D -s /srv -d public/)
 git add --all
 git commit -m 'Update and publish'
 git push origin master
